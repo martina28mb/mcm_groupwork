ce18f8c05d61454e718e1926c81b0dcfcf59d7ae
BailableCeck <61782276+BailableCeck@users.noreply.github.com>
2024-01-18 22:23:09 +0100

    Add checkbox filtering and result features

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,13 @@ def read_root():
 
 @app.get('/query/{person_name}')
 def read_item(
-    comune: str
+    comune: str,
+    piscina: Optional[bool] = Query(None),
+    accesso_disabili: Optional[bool] = Query(None),
+    fitness: Optional[bool] = Query(None),
+    sauna: Optional[bool] = Query(None),
+    aria_condizionata: Optional[bool] = Query(None),
+    lago: Optional[bool] = Query(None)
 ):
     """
     Retrieve accommodation and museum information based on specified filters.
@@ -47,13 +53,45 @@ def read_item(
     comune = comune.upper()
 
     results = df[df['COMUNE'] == comune]
+
+    if piscina is not None and piscina:
+        results = results[results['PISCINA'] == 'Vero']
+
+    if accesso_disabili is not None and accesso_disabili:
+        results = results[results['ACCESSO AI DISABILI'] == 'Vero']
+
+    if fitness is not None and fitness:
+        results = results[results['FITNESS'] == 'Vero']
+
+    if sauna is not None and sauna:
+        results = results[results['SAUNA'] == 'Vero']
+
+    if aria_condizionata is not None and aria_condizionata:
+        results = results[results['ARIA CONDIZIONATA'] == 'Vero']
+
+    if lago is not None and lago:
+        results = results[results['LAGO'] == 'Vero']
+
+
     denominazione_alloggio = results['DENOMINAZIONE'].tolist()
+    link_alloggio = results['SITO WEB'].tolist()
+    indirizzo_alloggio = results['INDIRIZZO'].tolist()
+    numero_telefono = results['TELEFONO'].tolist()
+    indirizzo_email = results['EMAIL'].tolist()
     results_musei = df_musei[df_musei['Comune'] == comune]
     denominazione_musei = results_musei['Nome'].tolist()
 
     result_list = []
-    for nome in zip(denominazione_alloggio):
+    for nome, link, indirizzo, telefono, email in zip(denominazione_alloggio, link_alloggio, indirizzo_alloggio, numero_telefono, indirizzo_email):
         result_item = {"nome": nome}
+        if pd.notna(link):
+            result_item["link"] = link
+        if pd.notna(indirizzo):
+            result_item["indirizzo"] = indirizzo
+        if pd.notna(telefono):
+            result_item["telefono"] = telefono
+        if pd.notna(email):
+            result_item["email"] = email
 
         result_list.append(result_item)
 
